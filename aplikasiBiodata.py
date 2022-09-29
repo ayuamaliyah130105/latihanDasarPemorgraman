e5a4410a5ffdf42ddf0d01f312cdf3c67b3f1e25
ayuamaliyah130105 <114630158+ayuamaliyah130105@users.noreply.github.com>
2022-09-28 21:15:03 -0700

    aplikasi biodata

---
+++ aplikasiBiodata.py
@@ -0,0 +1,25 @@
+#aplikasi biodata create by Ayu
+
+# memasukan nama
+nama = input ("masukkan nama anda! ")
+
+# print / output nama
+print("Nama anda adalah : ",nama)
+
+# memasukan kelas
+kelas = input ("masukkan kelas anda! ")
+
+# print / output kelas
+print("Kelas anda adalah : ", kelas)
+
+# masukan prodi
+prodi = input ("masukkan prodi anda! ")
+
+# print / output prodi
+print("Prodi anda adalah : ", prodi)
+
+# masukan tempat tanggal lahir
+tempat_tanggal_lahir = input ("masukkan tempat tanggal lahir! ")
+
+#print / output tempat tanggal lahir
+print("Tempat Tanggal Lahir anda adalah :", tempat_tanggal_lahir)
