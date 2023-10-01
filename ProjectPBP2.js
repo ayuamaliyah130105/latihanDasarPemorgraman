49e83afae74990bc0c97b98792f1c736220b22d2
ayuamaliyah130105 <114630158+ayuamaliyah130105@users.noreply.github.com>
2023-10-01 23:46:18 +0700

    Mata Kuliah Pemrograman Berbasis Platfrom Sesi 2

---
+++ ProjectPBP2.js
@@ -0,0 +1,11 @@
+var FirstName
+var LastName
+var Age
+var isMarried
+
+FirstName = "Rahayu Amaliyah"
+LastName = "Kamis"
+Age = "18"
+isMarried = false   
+
+console.log(`Nama Saya adalah ${FirstName}+${LastName}, saya berusia ${Age} Tahun.`)
